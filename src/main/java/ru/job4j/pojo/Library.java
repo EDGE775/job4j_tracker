4ee9cd785498992b136ea250d7e384d1476cae42
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-01-02 23:13:27 +0300

    3. Массивы и модели. [#405387]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,32 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book bookOne = new Book("Clean code", 464);
+        Book bookTwo = new Book("Идеальный программист", 214);
+        Book bookThree = new Book("Грокаем алгоритмы", 242);
+        Book bookFour = new Book("Самоучитель по Java", 1213);
+        Book[] books = { bookOne, bookTwo, bookThree, bookFour};
+        for (int i = 0; i < books.length; i++) {
+            Book book = books[i];
+            System.out.printf("%s - %d\n", book.getName(), book.getPages());
+        }
+
+        System.out.println("\nПереставьте местами книги с индексом 0 и 3:");
+        Book temp = books[3];
+        books[3] = books[0];
+        books[0] = temp;
+        for (int i = 0; i < books.length; i++) {
+            Book book = books[i];
+            System.out.printf("%s - %d\n", book.getName(), book.getPages());
+        }
+
+        System.out.println("\nЦикл с выводом книг с именем \"Clean code\":");
+        for (int i = 0; i < books.length; i++) {
+            Book book = books[i];
+            if (book.getName().equals("Clean code")) {
+                System.out.printf("%s - %d\n", book.getName(), book.getPages());
+            }
+        }
+    }
+}

de82c45ae0ab1ed7c00a17d313c27e1598870227
Dmitry.Kh <dima.khlapov@mail.ru>
2021-01-05 02:40:03 +0300

    3. Массивы и модели. [#405387] Рефакторинг кода

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -6,26 +6,26 @@ public class Library {
         Book bookTwo = new Book("Идеальный программист", 214);
         Book bookThree = new Book("Грокаем алгоритмы", 242);
         Book bookFour = new Book("Самоучитель по Java", 1213);
-        Book[] books = { bookOne, bookTwo, bookThree, bookFour};
+        Book[] books = {bookOne, bookTwo, bookThree, bookFour};
         for (int i = 0; i < books.length; i++) {
             Book book = books[i];
-            System.out.printf("%s - %d\n", book.getName(), book.getPages());
+            System.out.printf("%s - %d" + System.lineSeparator(), book.getName(), book.getPages());
         }
 
-        System.out.println("\nПереставьте местами книги с индексом 0 и 3:");
+        System.out.println(System.lineSeparator() + "Переставьте местами книги с индексом 0 и 3:");
         Book temp = books[3];
         books[3] = books[0];
         books[0] = temp;
         for (int i = 0; i < books.length; i++) {
             Book book = books[i];
-            System.out.printf("%s - %d\n", book.getName(), book.getPages());
+            System.out.printf("%s - %d" + System.lineSeparator(), book.getName(), book.getPages());
         }
 
-        System.out.println("\nЦикл с выводом книг с именем \"Clean code\":");
+        System.out.println(System.lineSeparator() + "Цикл с выводом книг с именем \"Clean code\":");
         for (int i = 0; i < books.length; i++) {
             Book book = books[i];
             if (book.getName().equals("Clean code")) {
-                System.out.printf("%s - %d\n", book.getName(), book.getPages());
+                System.out.printf("%s - %d" + System.lineSeparator(), book.getName(), book.getPages());
             }
         }
     }

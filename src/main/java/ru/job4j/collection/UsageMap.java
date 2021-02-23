2d6d15dcf601c3dd1bc9ade4e76b2a3ff7ff7934
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-02-23 12:27:16 +0300

    0. Map, HashMap [#405302]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("pupkin@mail.ru", "Vasia Pupkin");
+        map.put("khlapov@mail.ru", "Dmitry Khlapov");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}

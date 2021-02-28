599a955fbe59d8852a7ccb80da1d153e63f3daf0
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-02-28 21:54:04 +0300

    0. Сортировка [#405292]

---
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+    @Test
+    public void whenAdvanceSortByName() {
+        List<Item> items = new ArrayList<>();
+        Item item1 = new Item("Борис Кристофорович");
+        Item item2 = new Item("Арнольд Борисович");
+        Item item3 = new Item("Янковский Ян");
+        Item item4 = new Item("Малой Альберт");
+        items.add(item1);
+        items.add(item2);
+        items.add(item3);
+        items.add(item4);
+        items.sort(new ItemNameAdvanceSorter());
+        List<String> result = new ArrayList<>();
+        for (Item item : items) {
+            result.add(item.getName());
+        }
+        List<String> expect = Arrays.asList("Арнольд Борисович", "Борис Кристофорович", "Малой Альберт", "Янковский Ян");
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenDescendSortByName() {
+        List<Item> items = new ArrayList<>();
+        Item item1 = new Item("Борис Кристофорович");
+        Item item2 = new Item("Арнольд Борисович");
+        Item item3 = new Item("Янковский Ян");
+        Item item4 = new Item("Малой Альберт");
+        items.add(item1);
+        items.add(item2);
+        items.add(item3);
+        items.add(item4);
+        items.sort(new ItemNameDescendSorter());
+        List<String> result = new ArrayList<>();
+        for (Item item : items) {
+            result.add(item.getName());
+        }
+        List<String> expect = Arrays.asList("Янковский Ян", "Малой Альберт", "Борис Кристофорович", "Арнольд Борисович");
+        assertThat(result, is(expect));
+    }
+}

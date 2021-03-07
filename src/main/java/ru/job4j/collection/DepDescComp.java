f91aa66be85a7ed40a03f59ac79ec196c4e9e533
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-03-07 19:12:49 +0300

    2. Отсортировать департаменты [#405284] рефакторинг кода

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -10,8 +10,7 @@ public class DepDescComp implements Comparator<String> {
         String[] o2Array = o2.split("/");
         if (o1Array[0].equals(o2Array[0])) {
             return o1.compareTo(o2);
-        } else {
-            return o2Array[0].compareTo(o1Array[0]);
         }
+        return o2Array[0].compareTo(o1Array[0]);
     }
 }

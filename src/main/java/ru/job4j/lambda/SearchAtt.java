5e30dd09d9d26ffb1b0f8a0c4c8448d3e9f1155e
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-03-07 21:00:43 +0300

    2.0. Встроенные функциональные интерфейсы. [#405120]

---
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -0,0 +1,37 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class SearchAtt {
+    private static List<Attachment> filter(List<Attachment> list, Function<Attachment, Boolean> func) {
+        List<Attachment> rsl = new ArrayList<>();
+        for (Attachment att : list) {
+            if (func.apply(att)) {
+                rsl.add(att);
+            }
+        }
+        return rsl;
+    }
+
+    public static List<Attachment> filterSize(List<Attachment> list) {
+        Function<Attachment, Boolean> func = new Function<Attachment, Boolean>() {
+            @Override
+            public Boolean apply(Attachment attachment) {
+                return attachment.getSize() > 100;
+            }
+        };
+        return filter(list, func);
+    }
+
+    public static List<Attachment> filterName(List<Attachment> list) {
+        Function<Attachment, Boolean> func = new Function<Attachment, Boolean>() {
+            @Override
+            public Boolean apply(Attachment attachment) {
+                return attachment.getName().contains("bug");
+            }
+        };
+        return filter(list, func);
+    }
+}

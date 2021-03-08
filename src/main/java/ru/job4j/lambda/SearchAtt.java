57acabbf588c83620f85d434b4cf403ee24ea56f
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-03-08 14:15:31 +0300

    2.0. Встроенные функциональные интерфейсы. [#405120] рефакторинг кода

--- src/main/java/ru/job4j/lambda/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -2,14 +2,14 @@ package ru.job4j.lambda;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class SearchAtt {
     private static List<Attachment> filter(List<Attachment> list,
-                                           Function<Attachment, Boolean> func) {
+                                           Predicate<Attachment> func) {
         List<Attachment> rsl = new ArrayList<>();
         for (Attachment att : list) {
-            if (func.apply(att)) {
+            if (func.test(att)) {
                 rsl.add(att);
             }
         }
@@ -17,9 +17,9 @@ public class SearchAtt {
     }
 
     public static List<Attachment> filterSize(List<Attachment> list) {
-        Function<Attachment, Boolean> func = new Function<Attachment, Boolean>() {
+        Predicate<Attachment> func = new Predicate<Attachment>() {
             @Override
-            public Boolean apply(Attachment attachment) {
+            public boolean test(Attachment attachment) {
                 return attachment.getSize() > 100;
             }
         };
@@ -27,9 +27,9 @@ public class SearchAtt {
     }
 
     public static List<Attachment> filterName(List<Attachment> list) {
-        Function<Attachment, Boolean> func = new Function<Attachment, Boolean>() {
+        Predicate<Attachment> func = new Predicate<Attachment>() {
             @Override
-            public Boolean apply(Attachment attachment) {
+            public boolean test(Attachment attachment) {
                 return attachment.getName().contains("bug");
             }
         };

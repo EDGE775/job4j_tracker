71156b7cb5eecbb775c07bde89206d27e101245f
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-03-10 22:41:38 +0300

    0. Stream API [#405126]

---
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+    List<Integer> list = List.of(1, 3, -6, 7, -10);
+    List<Integer> numbers = new ArrayList<>(list);
+    List<Integer> fltNumbers = numbers.stream()
+            .filter(number -> number > 0)
+            .collect(Collectors.toList());
+
+}

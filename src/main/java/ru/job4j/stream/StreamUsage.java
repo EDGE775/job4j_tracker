02f5886b76cf3c4c55d510fd1947d998ff8ef84f
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-03-11 22:21:53 +0300

    0. Stream API [#405126] рефакторинг кода

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -5,10 +5,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamUsage {
-    List<Integer> list = List.of(1, 3, -6, 7, -10);
-    List<Integer> numbers = new ArrayList<>(list);
-    List<Integer> fltNumbers = numbers.stream()
-            .filter(number -> number > 0)
-            .collect(Collectors.toList());
-
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 3, -6, 7, -10);
+        List<Integer> numbers = new ArrayList<>(list);
+        List<Integer> fltNumbers = numbers.stream()
+                .filter(number -> number > 0)
+                .collect(Collectors.toList());
+    }
 }

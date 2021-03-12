e46c1deabce6c91e5e64e3dd1140a33e4ced19e3
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-03-13 00:53:45 +0300

    3. Уникальность элементов и сортировка. [#405129]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -29,4 +29,33 @@ public class ProfilesTest {
         List<Address> result = Profiles.collect(profiles);
         assertTrue(result.isEmpty());
     }
+
+    @Test
+    public void whenTheSameAddress() {
+        List<Profile> profiles = new ArrayList<>();
+        profiles.add(new Profile(new Address("Москва", "Юности", 1, 12)));
+        profiles.add(new Profile(new Address("Тула", "Проспект мира", 12, 41)));
+        profiles.add(new Profile(new Address("Тула", "Проспект мира", 12, 41)));
+        List<Address> result = Profiles.collect(profiles);
+        List<Address> expect = List.of(
+                new Address("Москва", "Юности", 1, 12),
+                new Address("Тула", "Проспект мира", 12, 41)
+        );
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSortedAddress() {
+        List<Profile> profiles = new ArrayList<>();
+        profiles.add(new Profile(new Address("Москва", "Юности", 1, 12)));
+        profiles.add(new Profile(new Address("Тула", "Проспект мира", 12, 41)));
+        profiles.add(new Profile(new Address("Архангельск", "Проспект инжира", 12, 41)));
+        List<Address> result = Profiles.collect(profiles);
+        List<Address> expect = List.of(
+                new Address("Архангельск", "Проспект инжира", 12, 41),
+                new Address("Москва", "Юности", 1, 12),
+                new Address("Тула", "Проспект мира", 12, 41)
+        );
+        assertThat(result, is(expect));
+    }
 }

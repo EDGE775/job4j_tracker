110617fbbba1624e1c3ec33ab2bc4c5a5a5cc51c
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-03-17 23:18:35 +0300

    1. Метод .of [#405138]

--- src/test/java/ru/job4j/lambda/FuncDiapasonTest.java
+++ src/test/java/ru/job4j/lambda/FuncDiapasonTest.java
@@ -13,21 +13,21 @@ public class FuncDiapasonTest {
     @Test
     public void whenLinearFunction() {
         List<Double> result = FuncDiapason.diapason(5, 8, x -> 2 * x + 1);
-        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        List<Double> expected = List.of(11D, 13D, 15D);
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenQuadraticFunction() {
         List<Double> result = FuncDiapason.diapason(5, 8, x -> x * x);
-        List<Double> expected = Arrays.asList(25D, 36D, 49D);
+        List<Double> expected = List.of(25D, 36D, 49D);
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenExponentialFunction() {
         List<Double> result = FuncDiapason.diapason(5, 8, x -> Math.pow(x, x));
-        List<Double> expected = Arrays.asList(3125D, 46656D, 823543D);
+        List<Double> expected = List.of(3125D, 46656D, 823543D);
         assertThat(result, is(expected));
     }
 }

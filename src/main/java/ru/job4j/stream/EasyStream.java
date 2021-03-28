592219872c827477d632bd972811b8a2af323c69
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-03-28 18:08:30 +0300

    EasyStream [#401629]

---
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -0,0 +1,54 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EasyStream {
+    private List<Integer> values;
+
+    private EasyStream() {
+
+    }
+
+    public static EasyStream of(List<Integer> source) {
+        EasyStream easyStream = new EasyStream();
+        easyStream.values = source;
+        return easyStream;
+    }
+
+    public EasyStream map(Function<Integer, Integer> fun) {
+        List<Integer> rsl = new ArrayList<>();
+        for (Integer value : values) {
+            rsl.add(fun.apply(value));
+        }
+        this.values = rsl;
+        return this;
+    }
+
+    public EasyStream filter(Predicate<Integer> fun) {
+        List<Integer> rsl = new ArrayList<>();
+        for (Integer value : values) {
+            if (fun.test(value)) {
+                rsl.add(value);
+            }
+        }
+        this.values = rsl;
+        return this;
+    }
+
+    public List<Integer> collect() {
+        return values;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> values = List.of(2, 5, 2, 1, 5, 1);
+        System.out.println(values);
+        List<Integer> rsl = EasyStream.of(values)
+                .map(x -> x * 2)
+                .filter(x -> x > 2)
+                .collect();
+        System.out.println(rsl);
+    }
+}
